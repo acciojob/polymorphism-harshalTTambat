b48be5fd664beaa02877ccdb6ae1c26937fc660a
Harshal Tambat <harshaltambat38@gmail.com>
2022-12-11 01:55:47 +0530

    method overloding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
-public class Main {
+public class Main
+{
+    static class Product
+    {
+        public int product(int x, int y)
+        {
+            return x * y;
+        }
+        public int product(int x, int y, int z)
+        {
+            return x * y * z;
+        }
+        public double product(double x, double y)
+        {
+            return x * y;
+        }
+    }
 
+    public static void main(String[] args)
+    {
+        Product p = new Product();
+        System.out.println(p.product( 10, 5));
+        System.out.println(p.product(6, 9, 2));
+        System.out.println(p.product(5.5, 5));
+    }
 }
